25c869d472ac45cdd59ba1a281238db1bcb4facc
narandza <60319154+narandza@users.noreply.github.com>
2025-05-31 16:06:09 +0200

    update globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Progress } from "@radix-ui/react-progress";
 
 export default function Home() {
   return (
-    <div className="">
-      <p className="text-blue-400">TEST</p>
-      <Button> i am a button</Button>
+    <div className="flex flex-col gap-4 items-center p-4">
+      <div className="">
+        <Button> i am a button</Button>
+      </div>
+      <div className="">
+        <Input placeholder="input" />
+      </div>
+      <div className="">
+        <Textarea> i am a textarea</Textarea>
+      </div>
+      <div className="">
+        <Progress></Progress>
+      </div>
+      <div className="">
+        <Checkbox>checkbox </Checkbox>
+      </div>
     </div>
   );
 }
