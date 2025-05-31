22314c6c269bbefb52cc93878589ff4c5ee79a72
narandza <60319154+narandza@users.noreply.github.com>
2025-05-31 15:59:39 +0200

    fix tailwind issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="">
+      <p className="text-blue-400">TEST</p>
+      <Button> i am a button</Button>
+    </div>
+  );
 }
