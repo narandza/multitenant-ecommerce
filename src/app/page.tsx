89d0e5639d13ab90dc5e2a6185f55cbe53df58ca
narandza <60319154+narandza@users.noreply.github.com>
2025-06-01 15:47:35 +0200

    customize  progress component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
-import { Progress } from "@radix-ui/react-progress";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 items-center p-4">
+    <div className="flex flex-col gap-4  p-4 w-full ">
       <div className="">
         <Button variant="elevated"> i am a button</Button>
       </div>
@@ -16,8 +16,8 @@ export default function Home() {
       <div className="">
         <Textarea> i am a textarea</Textarea>
       </div>
-      <div className="">
-        <Progress></Progress>
+      <div className=" ">
+        <Progress value={50} />
       </div>
       <div className="">
         <Checkbox>checkbox </Checkbox>
