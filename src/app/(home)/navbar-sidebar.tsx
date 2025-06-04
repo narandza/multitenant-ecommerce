0c6a4f03e850cfb244587bbad1d4af6fce4ca20f
narandza <60319154+narandza@users.noreply.github.com>
2025-06-04 10:42:42 +0200

    add sidebar menu items

--- src/app/(home)/navbar-sidebar.tsx
+++ src/app/(home)/navbar-sidebar.tsx
@@ -1,9 +1,11 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import Link from "next/link";
 
 interface NavbarItem {
   href: string;
@@ -21,10 +23,34 @@ export const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent side="left" className="p-0 transition-none">
         <SheetHeader className="p-4 border-b">
-          <div className="flex items-center">
-            <SheetTitle>Menu</SheetTitle>
-          </div>
+          <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
+
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+            >
+              {item.children}
+            </Link>
+          ))}
+          <div className="border-t">
+            <Link
+              href="/sign-in"
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+            >
+              Login
+            </Link>
+            <Link
+              href="/sign-up"
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+            >
+              Start selling
+            </Link>
+          </div>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
