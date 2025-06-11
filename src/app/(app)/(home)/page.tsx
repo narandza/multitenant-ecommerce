69d16ce64192cbdfd0d31fbd396857044be55d02
narandza <60319154+narandza@users.noreply.github.com>
2025-06-11 12:08:12 +0200

    test payload data load

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,27 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col gap-4  p-4 w-full ">
-      <div className="">
-        <Button variant="elevated"> i am a button</Button>
-      </div>
-      <div className="">
-        <Input placeholder="input" />
-      </div>
-      <div className="">
-        <Textarea> i am a textarea</Textarea>
-      </div>
-      <div className=" ">
-        <Progress value={50} />
-      </div>
-      <div className="">
-        <Checkbox>checkbox </Checkbox>
-      </div>
-    </div>
-  );
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+  });
+  return <div className="">{JSON.stringify(data, null, 2)}</div>;
 }
